3b907fad8ea9f48160f94e402e2f2c5a6064ce0e
Jason Plumb <jplumb@splunk.com>
2026-03-02 13:38:40 -0800

    setup

---
+++ run-collector.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+docker run -it --rm \
+    -p 4317:4317 \
+    -p 4318:4318 \
+    -v $(pwd)/collector.yaml:/app/collector.yaml \
+    otel/opentelemetry-collector-contrib \
+    --config /app/collector.yaml
---
+++ run-petclinic.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export OTEL_RESOURCE_ATTRIBUTES=deployment.environment=snpashot-repro
+export SPLUNK_SNAPSHOT_PROFILER_ENABLED=true
+
+java -Xmx512m -javaagent:./splunk-otel-javaagent-2.25.0.jar \
+	-Dotel.service.name=spring-petclinic-rest \
+    -Dotel.javaagent.debug=true \
+    -Dsplunk.snapshot.profiler.enabled=true \
+    -Dsplunk.snapshot.selection.rate=.1 \
+    -jar spring-petclinic-rest-3.2.1.jar 2>&1 | tee out.txt
